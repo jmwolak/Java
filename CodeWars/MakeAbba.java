ee27ff95577559aeae8ac0e3cb2f70da0ffb8939
Jim Wolak <54896873+jmwolak@users.noreply.github.com>
2020-05-27 15:05:54 -0400

    Add files via upload

---
+++ CodeWars/MakeAbba.java
@@ -0,0 +1,17 @@
+
+public class MakeAbba {
+
+	public static String makeAbba(String str1, String str2) {
+		
+		
+		return str1 + str2 + str2 + str1;
+		
+	}
+	public static void main(String[] args) {
+
+		System.out.println(makeAbba("Hi", "Bye"));
+		System.out.println(makeAbba("Yo", "Alice"));
+		System.out.println(makeAbba("What", "Up"));
+	}
+
+}
